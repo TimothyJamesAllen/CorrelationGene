bcc29f16fc4ed54ea499811c44326c72b2138608
Timothy Allen <125665567+VikingSOS1@users.noreply.github.com>
2023-12-08 13:53:34 +0100

    0.1.2 update

--- R/express_cell_RNA.r
+++ R/express_cell_RNA.r
@@ -11,13 +11,19 @@
 #' 
 #'
 #' @export
-express_cell_RNA = function(obj, goi, gene_list) {
+express_cell_RNA = function(obj, goi, gene_list, PorS) {
     merged = data.frame()
     tic()
     for (celltype in levels(obj)) {
         cat("Processing celltype: ", celltype, "into a dataframe", "\n")
         celltype_df_a = celltype_expression_RNA(obj, celltype, goi, gene_list)
-        celltype_df_a = correlation(celltype_df_a, goi, gene_list)
+        if (PorS == "P") {
+            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "P")
+        } else if (PorS == "S") {
+            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "S")
+        } else {
+            cat("Error: PorS must be either P or S")
+        }
         cat("Removing ", goi, " from celltype: ", celltype, "\n")
         celltype_a = celltype_df_a[!grepl(goi, celltype_df_a$gene),]
         cat("Adding celltype column...\n")
