dbc9a3bf081e2e2620e0c00dedf0f2880e6bbea7
Timothy Allen <tja21@ic.ac.uk>
2024-01-25 16:19:25 +0100

    fixing

--- R/express_cell_RNA.r
+++ R/express_cell_RNA.r
@@ -2,7 +2,7 @@
 #'
 #' 
 #'
-#' @param obj Seurat object. gene_list Gene list. goi Gene of interest.
+#' @param obj Seurat object. gene_list Gene list. goi Gene of interest. ensembl boolean
 #' 
 #' @return data frame with correlation values
 #'
@@ -11,16 +11,16 @@
 #' 
 #'
 #' @export
-express_cell_RNA = function(obj, goi, gene_list, PorS) {
+express_cell_RNA = function(obj, goi, gene_list, PorS, ensembl) {
     merged = data.frame()
     tic()
     for (celltype in levels(obj)) {
         cat("Processing celltype: ", celltype, "into a dataframe", "\n")
         celltype_df_a = celltype_expression_RNA(obj, celltype, goi, gene_list)
         if (PorS == "P") {
-            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "P")
+            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "P", ensembl)
         } else if (PorS == "S") {
-            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "S")
+            celltype_df_a <- correlation(celltype_df_a, goi, gene_list, "S", ensembl)
         } else {
             cat("Error: PorS must be either P or S")
         }
