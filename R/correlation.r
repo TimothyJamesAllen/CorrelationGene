e540f2d19610eb3a080bdf8a70c5fd5bf997fd1b
Timothy Allen <tja21@ic.ac.uk>
2024-01-25 15:55:06 +0100

    Fixing

--- R/correlation.r
+++ R/correlation.r
@@ -7,17 +7,18 @@
 #' @param PorS Type of correlation to compute (P for Pearson, S for Spearman)
 #' @param goi Gene of interest.
 #' @param gene_list Gene list.
+#' @param ensembl Boolean, whether to convert Ensembl IDs to gene symbols 
 #' 
 #' 
 #' @return A matrix of correlation values
 #'
 #' @examples
-#' correlation(df, goi, gene_list, PorS)
-#' correlation(matrix,"RFX3", gene_list, "P")
+#' correlation(df, goi, gene_list, PorS, ensembl)
+#' correlation(matrix,"RFX3", gene_list, "P", ensembl = "FALSE")
 #' 
 #'
 #' @export
-correlation = function(df, goi, gene_list, PorS) {
+correlation = function(df, goi, gene_list, PorS, ensembl) {
 
   cat("Computing correlation matrix...\n")
   if (PorS == "P") {
@@ -40,20 +41,35 @@ correlation = function(df, goi, gene_list, PorS) {
   merged = merged[order(merged$R, decreasing = TRUE), ]
   cat("Merging data frames...\n")
   common_genes = intersect(merged$gene, gene_list)
+  merged = merged[merged$gene %in% common_genes, ]
 
 
-cat("Adding gene annotations...\n")
-ensembl <- mapIds(org.Hs.eg.db, keys = common_genes$gene, column = "ENSEMBL", keytype = "SYMBOL", multiVals = "first")
 
-colnames(ensembl) <- c("ensembl_gene_id", "external_gene_name")
+  if (ensembl == TRUE) {
+    cat("converting ensemble IDs to gene symbols...\n")
+    symbols <- AnnotationDbi::select(org.Hs.eg.db, keys = merged$gene,
+                                 columns = "SYMBOL",
+                                 keytype = "ENSEMBL")
 
-merged <- merge(merged, ensembl, by.x = "gene", by.y = "ensembl_gene_id")
+  }
+
+  merged <- merge(merged, symbols, by.x = "gene", by.y = "ENSEMBL")
+
+# Remove the original Ensembl gene ID column
+  merged <- merged[, !(names(merged) %in% "gene")]
+
+# Rename the new gene symbol column
+  colnames(merged)[colnames(merged) == "SYMBOL"] <- "gene_symbol"
+
+
+  cat("Done.\n")
+
 
 # Loop through each gene ID in the merged data frame
 
 for (i in 1:nrow(merged)) {
   n <- merged$external_gene_name[i]
-  if (n %in% SFARI_gene$gene.symbol) {
+  if (n %in% SFARI_genes$gene.symbol) {
     merged$SFARI.Gene[i] <- "TRUE"
   } else {
     merged$SFARI.Gene[i] <- "FALSE"
