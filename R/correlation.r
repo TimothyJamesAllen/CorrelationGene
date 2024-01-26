021877b9eba5695b0efaed5f80b8b907661c7c17
Timothy Allen <tja21@ic.ac.uk>
2024-01-26 10:14:12 +0100

    Update correlation.r

--- R/correlation.r
+++ R/correlation.r
@@ -19,9 +19,10 @@
 #'
 #' @export
 correlation = function(df, goi, gene_list, PorS, ensembl) {
-
+  
   cat("Computing correlation matrix...\n")
-    if (nrow(df) > 4) {
+  
+  if (nrow(df) > 4) {
     if (PorS == "P") {
       df <- rcorr(as.matrix(df), type = "pearson")
     } else if (PorS == "S") {
@@ -31,48 +32,37 @@ correlation = function(df, goi, gene_list, PorS, ensembl) {
       return(NULL)
     }
     
-  df_r = as.data.frame(df$r)
-  df_p = as.data.frame(df$P)
-  cat("extracting data...\n")
-  goi_only_p = as.data.frame(df_p[goi], row.names = rownames(df_p))
-  goi_only_r = as.data.frame(df_r[goi], row.names = rownames(df_r))
-  cat("adding gene names...\n")
-  goi_only_p$gene = rownames(goi_only_p)
-  goi_only_r$gene = rownames(goi_only_r)
-  merged = merge(goi_only_r, goi_only_p, by = "gene")
-  colnames(merged)[2:3] <- c("R","P")  
-  merged = merged[order(merged$R, decreasing = TRUE), ]
-  cat("Merging data frames...\n")
-  common_genes = intersect(merged$gene, gene_list )
-  merged = merged[merged$gene %in% common_genes, ]
-
-
-
-  if (ensembl == TRUE) {
-    cat("converting ensemble IDs to gene symbols...\n")
-    symbols <- AnnotationDbi::select(org.Hs.eg.db, keys = merged$gene,
-                                 columns = "SYMBOL",
-                                 keytype = "ENSEMBL")
-
-
-    merged <- merge(merged, symbols, by.x = "gene", by.y = "ENSEMBL")
-
+    df_r = as.data.frame(df$r)
+    df_p = as.data.frame(df$P)
+    cat("extracting data...\n")
+    goi_only_p = as.data.frame(df_p[goi], row.names = rownames(df_p))
+    goi_only_r = as.data.frame(df_r[goi], row.names = rownames(df_r))
+    cat("adding gene names...\n")
+    goi_only_p$gene = rownames(goi_only_p)
+    goi_only_r$gene = rownames(goi_only_r)
+    merged = merge(goi_only_r, goi_only_p, by = "gene")
+    colnames(merged)[2:3] <- c("R","P")  
+    merged = merged[order(merged$R, decreasing = TRUE), ]
+    cat("Merging data frames...\n")
+    common_genes = intersect(merged$gene, gene_list )
+    merged = merged[merged$gene %in% common_genes, ]
+    
+    cat("Adding SFARI Gene column...\n", "make sure your gene column is named 'SYMBOL'\n")
+    for (i in 1:nrow(merged)) {
+      n <- merged$SYMBOL[i]
+      if (n %in% SFARI_genes$gene.symbol) {
+        merged$SFARI.Gene[i] <- "TRUE"
+      } else {
+        merged$SFARI.Gene[i] <- "FALSE"
+      }
+    }
+    
     cat("Done.\n")
-
-  }
-
-# Loop through each gene ID in the merged data frame
-cat("Adding SFARI Gene column...\n", "make sure your gene column is names SYMBOL\n")
-for (i in 1:nrow(merged)) {
-  n <- merged$SYMBOL[i]
-  if (n %in% SFARI_genes$gene.symbol) {
-    merged$SFARI.Gene[i] <- "TRUE"
+    
+    return(merged)
+    
   } else {
-    merged$SFARI.Gene[i] <- "FALSE"
+    cat("Error: Not enough observations to compute correlation matrix\n")
+    return(NULL)
   }
-}
-
-cat("Done.\n")
-
-return(merged)
 }
